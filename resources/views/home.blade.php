274ba791866774ac39b2d52ba4753ec4cbc2d25e
Vinícius Portes <viniciusportescn@gmail.com>
2023-09-13 16:58:35 -0300

    esqueleto do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="{{ asset('home.css') }}">
+</head>
+<body>
+    <div class="topnav">
+        <a>Home</a>
+        <a>Posts</a>
+        <a>Contact</a>
+        <a>About</a>
+    </div>
+    <h1 class="center">Página Inicial</h1>
+    <div id="fullscreen-img">
+    </div>
+</body>
+</html>
